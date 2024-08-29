3e5c356feb30694a9d1dc26e607185e811603735
Win10kc <minhht.ba12-126@st.usth.edu.vn>
2024-08-30 00:29:57 +0700

    create ForecastFragment include layout, set backgroud

--- USTHWeather/app/src/main/java/com/example/usthweather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/com/example/usthweather/WeatherActivity.java
@@ -1,57 +1,61 @@
 package com.example.usthweather;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.activity.EdgeToEdge;
+//import android.util.Log;
+//import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class WeatherActivity extends AppCompatActivity {
-    private static final String TAG = "WeatherActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        Log.i(TAG, "onCreate called");
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart called");
-    }
+        // Check if the fragment container is available
+        if (findViewById(R.id.fragment_container) != null) {
+            // Avoid overlapping fragments when activity is recreated
+            if (savedInstanceState != null) {
+                return;
+            }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume called");
-    }
+            // Create a new instance of ForecastFragment
+            ForecastFragment forecastFragment = new ForecastFragment();
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(TAG, "onPause called");
-    }
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, forecastFragment).commit();
+        }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy called");
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Log.i(TAG, "onStart called");
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Log.i(TAG, "onResume called");
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Log.i(TAG, "onPause called");
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Log.i(TAG, "onStop called");
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Log.i(TAG, "onDestroy called");
     }
 }
